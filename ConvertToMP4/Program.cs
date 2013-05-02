a9356b3f80e68064aecadb3b398bf2383db7a905
Dror Levy <doru@animeholic.net>
2013-05-02 05:26:08 +0300

    switched to using ffmpeg for all operations

--- ConvertToMP4/Program.cs
+++ ConvertToMP4/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConvertToMP4
@@ -12,13 +13,16 @@ namespace ConvertToMP4
     {
         static void Main(string[] args)
         {
+            string tmp;
             //string command = "-i \"{0}\" -s 740x416 -c:v libx264 -crf:v 26 -preset:v medium -ac 1 -c:a aac -strict -2 -cutoff 15000 -b:a 64k \"{1}\"";
             string command = "-i \"{0}\" -s 740x416 -c:v libx264 -crf:v 26 -preset:v medium -ac 1 -c:a aac -strict -2 -cutoff 15000 -b:a 64k \"{1}\"";
-            command = "-y -i \"{0}\" -s 640x360 -c:v libx264 -crf:v 26 -preset:v veryslow -ac 1 -c:a libfdk_aac -b:a 64k -strict -2 -cutoff 15000 -vf \"ass='{2}'\" \"{1}\"";
+            command = "-y -i \"{0}\" -s 640x360 -c:v libx264 -crf:v 26 -preset:v veryfast -ac 1 -c:a libfdk_aac -b:a 64k -strict -2 -cutoff 15000 -vf \"ass='{2}'\" \"{1}\"";
             List<string> cleanup = new List<string>();
-            
+
             foreach (var item in args)
             {
+                FileInfo fileinfo = new FileInfo(item);
+                var dir = Path.GetDirectoryName(item);
                 List<string> fileParts = item.Split('.').ToList();
                 string filename = "";
                 fileParts.Insert(fileParts.Count - 1, "MOBILE");
@@ -26,50 +30,96 @@ namespace ConvertToMP4
                 fileParts.Add("mp4");
                 filename = string.Join(".", fileParts.ToArray());
 
-                string mkvmergeOutput = ExecuteAndReturn("mkvmerge", "--identify \"" + item + "\"");
+                //string mkvmergeOutput = ExecuteAndReturn("mkvmerge", "--identify \"" + item + "\"");
 
-                string[] mkvmergeLines = mkvmergeOutput.Split(new string[]{System.Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries);
+                //string[] mkvmergeLines = mkvmergeOutput.Split(new string[]{System.Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries);
                 int SubtitleTrack = -1;
                 string subFormat = "";
-                foreach (var line in mkvmergeLines)
+
+
+                string subtitlefile = filename + ".ass";
+
+                //foreach (var line in mkvmergeLines)
+                //{
+                //    if (line.IndexOf("TEXT/SSA") != -1 || line.IndexOf("TEXT/SRT") != -1 || line.IndexOf("TEXT/ASS") != -1)
+                //    {
+                //        if (line.IndexOf("TEXT/SSA") != -1)
+                //            subFormat = "SSA";
+                //        if (line.IndexOf("TEXT/ASS") != -1)
+                //            subFormat = "ASS";
+                //        if (line.IndexOf("TEXT/SRT") != -1)
+                //            subFormat = "SRT";
+                //        SubtitleTrack = int.Parse(line.Substring(9, line.IndexOf(':') - 9));
+                //        break;
+                //    }
+                //}
+
+                //Regex r = new Regex("Attachment ID ([0-9]*): type 'application/.+font', size [0-9]+ bytes, file name '(.+)'");
+                ////System.Diagnostics.Debugger.Break();
+
+                //foreach (var line in mkvmergeLines)
+                //{
+                //    var result = r.Match(line);
+                //    if (result.Success)
+                //    {
+                //        ExecuteAndReturn("mkvextract", string.Format("attachments \"{0}\" {1}:\"{2}\"", item, result.Groups[1].Value, result.Groups[2].Value));
+                //        if (!File.Exists("C:\\tools\\ffmpeg\\fonts\\" + result.Groups[2].Value))
+                //            File.Copy(result.Groups[2].Value, "C:\\tools\\ffmpeg\\fonts\\" + result.Groups[2].Value);
+                //        cleanup.Add(result.Groups[2].Value);
+                //    }
+
+                //}
+
+                tmp = ExecuteAndReturn("ffmpeg", string.Format(" -i \"{0}\"", item));
+                Regex r = new Regex("Input #0, .+?, from '" + Regex.Escape(item) + "':.+", RegexOptions.Singleline);
+
+                var result = r.Match(tmp);
+                tmp = result.Value;
+                r = new Regex("\\s{4}Stream #0:(?<TrackID>[0-9])+(\\((?<language>[^()]+)\\))?: (?<Type>Video|Audio|Subtitle|Attachment):[^\\r\\n]+\\r\\n(\\s{4}Metadata:\\r\\n\\s{6}filename\\s*:\\s*(?<FileName>[^\\r\\n]+)\\r\\n\\s{6}mimetype[^\\r\\n]+)?", RegexOptions.Singleline);
+                var resultCollection = r.Matches(tmp);
+                //Stream #0:0: Video: mpeg4 (XVID / 0x44495658), yuv420p, 640x480, 23.98 fps, 23.98 tbr, 23.98 tbn, 23.98 tbc
+                //Stream #0:1(English[eng]): Audio: vorbis, 48000 Hz, stereo, fltp, 176 kb/s
+                //Stream #0:2(Japanese[jpn]): Audio: vorbis, 48000 Hz, stereo, fltp, 176 kb/s
+                //Stream #0:3(English[eng]): Subtitle: text
+                Match[] a = new Match[resultCollection.Count];
+                resultCollection.CopyTo(a, 0);
+
+                string audioStream = "a";
+                if (a.Count(g => g.Groups["Type"].Value == "Audio") > 1)
                 {
-                    if (line.IndexOf("TEXT/SSA") != -1 || line.IndexOf("TEXT/SRT") != -1 || line.IndexOf("TEXT/ASS") != -1)
+                    var audioResult = a.Where(g => g.Groups["Type"].Value == "Audio").FirstOrDefault(g => g.Value.Contains("jpn") || g.Value.Contains("Japanese"));
+                    if (audioResult != null)
+                        audioStream = audioResult.Groups["TrackID"].Value;
+                }
+                bool hasSubs = false;
+
+                var subtitleResult = a.FirstOrDefault(g => g.Groups["Type"].Value == "Subtitle");
+                if (subtitleResult != null)
+                {
+                    ExecuteAndReturn("ffmpeg", string.Format("-i \"{0}\" -y  -vn -an -codec:s:0.{1} ssa  \"{2}\"", item, subtitleResult.Groups["TrackID"].Value, subtitlefile));//-codec:s:0.{1}
+                    hasSubs = true;
+                    cleanup.Add(subtitlefile);
+                }
+
+                if (a.Count(g => g.Groups["Type"].Value == "Attachment") > 0)
+                {
+                    ExecuteAndReturn("ffmpeg", string.Format("-dump_attachment:t \"\" -i \"{0}\" -y", item), dir);
+
+                    foreach (var f in a.Where(g => g.Groups["Type"].Value == "Attachment").Select(g => g.Groups["FileName"].Value))
                     {
-                        if (line.IndexOf("TEXT/SSA") != -1)
-                            subFormat = "SSA";
-                        if (line.IndexOf("TEXT/ASS") != -1)
-                            subFormat = "ASS";
-                        if (line.IndexOf("TEXT/SRT") != -1)
-                            subFormat = "SRT";
-                        SubtitleTrack = int.Parse(line.Substring(9, line.IndexOf(':') - 9));
-                        break;
+                        cleanup.Add(dir + "\\" + f);
+                        if (!File.Exists("C:\\tools\\ffmpeg\\fonts\\" + f))
+                            File.Copy(dir+"\\"+f, "C:\\tools\\ffmpeg\\fonts\\" + f);
+
                     }
                 }
 
-                Regex r = new Regex("Attachment ID ([0-9]*): type 'application/.+font', size [0-9]+ bytes, file name '(.+)'");
-                //System.Diagnostics.Debugger.Break();
-
-                foreach (var line in mkvmergeLines)
-                {
-                    var result = r.Match(line);
-                    if (result.Success)
-                    {
-                        ExecuteAndReturn("mkvextract", string.Format("attachments \"{0}\" {1}:\"{2}\"", item, result.Groups[1].Value, result.Groups[2].Value));
-                        if (!File.Exists("C:\\tools\\ffmpeg\\fonts\\" + result.Groups[2].Value))
-                            File.Copy(result.Groups[2].Value, "C:\\tools\\ffmpeg\\fonts\\" + result.Groups[2].Value);
-                        cleanup.Add(result.Groups[2].Value);
-                    }
-
-                }
 
 
+                //ExecuteAndReturn("mkvextract", string.Format("tracks \"{0}\" {1}:\"{2}\"", item, SubtitleTrack.ToString(), subtitlefile));
+                subtitlefile = subtitlefile.Replace("\\", "\\\\").Replace(":", "\\:");
 
-                string subtitlefile = filename + "." + subFormat;
-                cleanup.Add(subtitlefile);
 
-                ExecuteAndReturn("mkvextract", string.Format("tracks \"{0}\" {1}:\"{2}\"", item, SubtitleTrack.ToString(), subtitlefile));
-                subtitlefile= subtitlefile.Replace("\\","\\\\").Replace(":","\\:");
-                
 
                 var process = new System.Diagnostics.Process();
 
@@ -87,11 +137,11 @@ namespace ConvertToMP4
                 //}
                 //string r = q.ToString();
 
-                
+
 
                 process.WaitForExit();
             }
-            
+
             Console.Read();
             foreach (var record in cleanup.Distinct())
             {
@@ -100,22 +150,31 @@ namespace ConvertToMP4
 
         }
 
-        static string ExecuteAndReturn(string command, string args)
+        static string ExecuteAndReturn(string command, string args,string startAt = null)
         {
             var process = new System.Diagnostics.Process();
 
 
             process.StartInfo = new System.Diagnostics.ProcessStartInfo(command, args);
+            if(startAt!=null)
+                process.StartInfo.WorkingDirectory = startAt;
             process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
             process.StartInfo.UseShellExecute = false;
-            //process.OutputDataReceived += process_OutputDataReceived;
+            process.OutputDataReceived += process_OutputDataReceived;
             process.Start();
             StringBuilder q = new StringBuilder();
             while (!process.HasExited)
             {
+                //if (process.StandardError.EndOfStream)
+                q.Append(process.StandardError.ReadToEnd());
+                //if (process.StandardOutput.EndOfStream)
                 q.Append(process.StandardOutput.ReadToEnd());
+
+                Thread.Sleep(0);
                 //Console.Write(process.StandardOutput.ReadToEnd());
             }
+
             return q.ToString();
         }
 
